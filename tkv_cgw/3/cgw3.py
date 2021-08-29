3c36f86a12c0fd48d0ff7f34c578f7b7a7553fb6
MrKozelberg <Mr.Kozelberg@yandex.ru>
2021-08-29 18:36:29 +0300

    nothing

--- tkv_cgw/3/cgw3.py
+++ tkv_cgw/3/cgw3.py
@@ -24,5 +24,6 @@ p1 = plot3d(A2, (n,0,20), (m,-50,50), nb_of_points_x=100, nb_of_points_y = 100)
 
 A2 = 2*sqrt(10)*A_0**2*exp(-alpha*omega_0**2*(2*m - n)**2*(2*Omega**2*(sqrt(2) + 2) - omega_0**2)/(alpha**2*n**2*(Omega**2*(1 + 3*sqrt(2)) - omega_0**2)**2 + 2*omega_0**2*(2*Omega**2*(sqrt(2) + 2) - omega_0**2)))/(5*omega_0*sqrt(alpha**2*n**2*(Omega**2*(1 + 3*sqrt(2)) - omega_0**2)**2 + 2*omega_0**2*(2*Omega**2*(sqrt(2) + 2) - omega_0**2))*(4*Omega**2 - omega_0**2))
 A2 = ((((A2.subs(n,0)).subs(Omega,1)).subs(alpha,1)).subs(A_0,1)).subs(m,0)
+
 #print('|A|^2 from omega_0')
 #p3 = plot(A2, (omega_0, 2-sqrt(2)+0.1, 2-sqrt(2)-0.1))
